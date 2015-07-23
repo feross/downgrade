6475c36a87fc4e61aa8dc08b6f65f889b5adcaab
Feross Aboukhadijeh <feross@feross.org>
2015-07-22 17:55:46 -0700

    fix for windows

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ module.exports = function downgrade (uid, gid) {
       process.getuid(), uid, process.getgid(), gid
     )
   } catch (err) {
+    var currentUid = (process.getuid && process.getuid()) || 'none'
+    var currentGid = (process.getgid && process.getgid()) || 'none'
     debug(
       'Failed to downgrade: uid=%s (desired=%s); gid=%s (desired=%s)',
-      process.getuid(), uid, process.getgid(), gid
+      currentUid, uid, currentGid, gid
     )
   }
 }
